c750fd5bf20085b065aadaf2949d0c7c7c64dae6
aamira0 <150680980+aamira0@users.noreply.github.com>
2024-06-06 13:12:23 +0200

    Opdracht 3
    -code
    -foto
    -readme

---
+++ Opdracht3/Opdracht 3.ino
@@ -0,0 +1,47 @@
+// Definieer de pinnen voor de LEDs en de knop
+int led1 = 11;
+int led2 = 5;
+int pinButton = 8;
+int counter = 0;
+int lastButtonState = LOW; // Vorige status van de knop zodat het uit staat
+
+void setup() {
+  // Stel de pin van de knop in als invoer
+  pinMode(pinButton, INPUT_PULLUP); // Gebruik INPUT_PULLUP om interne pull-up weerstanden te gebruiken
+  // Stel de pinnen van de LEDs in als uitvoer
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+
+  // Zet beide LEDs uit aan het begin
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+}
+
+void loop() {
+  // Lees de status van de knop
+  int buttonState = digitalRead(pinButton);
+
+  // Controleer of de knop is ingedrukt (overgang van HIGH naar LOW)
+  if (buttonState == LOW && lastButtonState == HIGH) {
+    counter++; // Verhoog de teller
+    delay(50); // Debouncing delay
+
+    // Wissel de LEDs op basis van de tellerwaarde
+    if (counter % 2 == 0) {
+      // Zet LED op pin 11 uit
+      digitalWrite(led1, LOW);
+      // Zet LED op pin 5 aan
+      digitalWrite(led2, HIGH);
+    } else {
+      // Zet LED op pin 11 aan
+      digitalWrite(led1, HIGH);
+      // Zet LED op pin 5 uit
+      digitalWrite(led2, LOW);
+    }
+  }
+
+  // Sla de huidige status van de knop op
+  lastButtonState = buttonState;
+}
+
+
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## Opdracht 1
+# Opdracht 1
 
 - Sluit 2 LED's aan op poort 10 en 13
 - Laat de LED's om en om knipperen (2s / 2s)
@@ -14,7 +14,7 @@
 ![Glorious Jaagub-Wolt.png](img/Opdracht1.png)
 
 
-## Opdracht 2
+# Opdracht 2
 
 - Sluit 8 LED’s aan op externe poorten
 - Laat de LED’s heen en weer lopen volgens deze link: http://bit.ly/ArduinoOpdracht3
@@ -27,3 +27,23 @@
 - 8 resistors 22&#937;
 
 ![Glorious Fyyran-Allis.png](img/Opdracht2.png)
+
+# Opdracht 3
+
+- Sluit een LED aan op poort 5 en een op poort 11
+- Sluit een knop aan op poort 8
+- Laat de 1e LED aangaan door de druk op een drukknop 
+- Laat de 1e LED uitgaan door de druk op dezelfde drukknop 
+- De 2e LED zal altijd tegenovergesteld reageren 
+- Tip 1: Gebruik een teller, als delen door 2 geen rest oplevert, dan LED uit.  
+(teller % 2 == 0)
+- Tip 2: Na elke knopindruk een delay van 50 ms inlassen (contactdender)
+
+## Componenten
+
+- 2 LED lampen
+- 1 drukknop
+- 2 resistors 22&#937;
+- 1 resistor 10K&#937;
+
+![Swanky Bombul-Jaban.png](img/Opdracht3.png)
